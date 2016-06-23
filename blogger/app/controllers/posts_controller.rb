44feae96c25121b1a4132ae761991824fc67c559
Enrique Carral Trevino <ecarralt@gmail.com>
2016-06-22 23:02:53 -0500

    All working except edit notice

--- blogger/app/controllers/posts_controller.rb
+++ blogger/app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     @post.title = params[:post][:title] # gets what user typed in for name
     @post.body = params[:post][:body] # gets what user typed in for name
     if @post.save # checks .valid? first
-      redirect_to posts_path
+      redirect_to posts_path, notice: "new post created!"
     else
       render :new
     end
